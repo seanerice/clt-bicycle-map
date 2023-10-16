d6559747510558657954e896df7f171b7c224630
Sean Rice <seane.rice@gmail.com>
2023-10-16 03:25:27 -0400

    WIP: add granular layer filters with legend

--- website/src/layer-widget.js
+++ website/src/layer-widget.js
@@ -27,6 +27,15 @@ export class LayerWidget extends LitElement {
             font-family: 'Open Sans', sans-serif;
             z-index: 1;
         }
+
+        .greenway-route-line {
+            display: inline-block;
+            width: 10px;
+            margin:5px 0;
+            height:2px;
+            background:
+              repeating-linear-gradient(90deg,red 0 5px,#0000 0 7px)
+        }
     `;
 
     render() {
@@ -41,6 +50,37 @@ export class LayerWidget extends LitElement {
                     checked
                 >
                 <label for="routes">Routes</label><br>
+                <ul>
+                    <li>
+                        <i class="greenway-route-line"></i>
+                        <input
+                            type="checkbox"
+                            id="greenway-routes"
+                            name="greenway-routes"
+                            checked
+                        >
+                        <label for="routes">Greenway Routes</label>
+                    </li>
+                    <li>
+                        <input
+                            type="checkbox"
+                            id="signed-routes"
+                            name="signed-routes"
+                            checked
+                        >
+                        <label for="routes">Signed Routes</label>
+                    </li>
+                    <li>
+                        <input
+                            type="checkbox"
+                            id="suggested-routes"
+                            name="suggested-routes"
+                            checked
+                        >
+                        <label for="routes">Suggested Routes</label>
+                    </li>
+                </ul>
+
                 <input
                     type="checkbox"
                     id="cycle-lanes"
