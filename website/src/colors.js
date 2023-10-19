8161ec9e2ec11ea5ad22e5cb1765073b4f071e63
Sean Rice <seane.rice@gmail.com>
2023-10-18 23:55:07 -0400

    Add filters for all cycle lanes (#19)

---
+++ website/src/colors.js
@@ -0,0 +1,25 @@
+export const bicycleFacilityRatingColor = {
+    'dangerous': '#DD6060',
+    'poor': '#F8A954',
+    'adequate': '#FFE83B',
+    'good': '#7CE647',
+    'great': '#0EAECB',
+    'excellent': '#1068DA'
+};
+
+export const roadwayPalette = {
+    cycleTrack: bicycleFacilityRatingColor.excellent,
+    bufferedLane: bicycleFacilityRatingColor.great,
+    lane: bicycleFacilityRatingColor.poor,
+    shareBusway: bicycleFacilityRatingColor.poor,
+    sharedLane: bicycleFacilityRatingColor.poor,
+    shoulder: bicycleFacilityRatingColor.dangerous,
+    none: '#a83295'
+};
+
+export const routePalette = {
+    greenway: '#3964C4',
+    signed: '#e6c627',
+    suggested: '#8539C4',
+    none: '#a83295'
+}
